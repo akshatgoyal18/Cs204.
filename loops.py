1a34b74b01b5eb6a9f3348e24b5118b95255e53a
akshatgoel <akshatgoyal18@gmail.com>
2020-07-19 17:10:43 +0530

    mew commit

--- loops.py
+++ loops.py
@@ -1,3 +1,4 @@
 print('new file has been created')
 print('the file has been updated for the first time')
-print('changes has been made in dev branch')
+print('change in dev branch')
+print('change in dev1 branch')
